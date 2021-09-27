3abae9d0e15ac6873ec2cd49a6a4331382389e09
93virus <bikasadhikari333@gmail.com>
2021-09-27 16:42:28 +0530

    build

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,7 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import Topbar from '../../components/topbar/Topbar';
 import Feed from '../../components/feed/Feed'
 import Rightbar from '../../components/rightbar/Rightbar'
-import './home.css'
+import 'home.css'
 
 const Home = () => {
     return (
